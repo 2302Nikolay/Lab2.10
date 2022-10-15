1844359f0b61e633346b9411ba2cb0ed5a6fd834
2302Nikolay <Malzewn@gmail.com>
2022-10-15 21:22:09 +0300

    Выполнил третье задание

--- Задания/ind_task.py
+++ Задания/ind_task.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*
+
+def print_prog_lang(**lang):
+    st = "-" * 40
+    print("Programming languages: \n")
+    print(st)
+    for field, lng in lang.items():
+        print(f"{field}:", " "*(10-len(field)), '|', lng)
+        print(st)
+
+
+if __name__ == "__main__":
+    print_prog_lang(
+        Science=["C++", "Fortran", "Java"],
+        AI=["Python", "Prolog", "LISP"],
+        Gamedev="C#"
+    )
