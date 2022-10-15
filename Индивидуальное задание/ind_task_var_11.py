b292072100a061bf514d95ddf33db9a16d63296d
2302Nikolay <Malzewn@gmail.com>
2022-10-15 21:51:50 +0300

    Выполнил индивидуальное задание

--- Индивидуальное задание/ind_task_var_11.py
+++ Индивидуальное задание/ind_task_var_11.py
@@ -2,5 +2,23 @@
 # -*- coding^ utf-8 -*-
 
 
+def sum_positive(*values):
+    """""
+    Сумму аргументов, расположенных 
+    после первого положительного аргумента.
+    """""
+    if values:
+        pos = 0
+        temp = 0
+        for i in values:
+                if i > 0:
+                    pos = i
+        for i in values[pos+1:]:
+            temp += i
+        return temp
+    else:
+        return None
+
+
 if __name__ == '__main__':
-    print("Hello!")
+    print(sum_positive(-1, -1, -2, -3, -4, -5, 6, 7, 8, 5, -1))
