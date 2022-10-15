6a1f907ee9827d969a4e78561c14bab60ee5c581
2302Nikolay <Malzewn@gmail.com>
2022-10-15 18:52:53 +0300

    Выполнил второе задание

--- Задания/task_1.py
+++ Задания/task_1.py
@@ -5,11 +5,14 @@ import math
 
 
 def geo_mean(*values):
-    temp = 1
-    for i in range(0, len(values)):
-        temp = temp * values[i]
-    return math.pow(temp, 1/len(values))
+    if values:
+        temp = 1
+        for i in range(0, len(values)):
+            temp = temp * values[i]
+        return math.pow(temp, 1/len(values))
+    else:
+        return None
 
 
 if __name__ == "__main__":
-    print(geo_mean(1, 2, 3, 4, 5, 6, 7, 8))
+    print(geo_mean(1, 2, 3, 4, 5, 6))
--- Задания/task_2.py
+++ Задания/task_2.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*
+
+
+def mean_gar(*values):
+    if values:
+        temp = 0
+        for i in range(0, len(values)):
+            temp = temp + 1/values[i]
+        return len(values)/temp
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    print(mean_gar(1, 2, 3, 4, 5, 6))
